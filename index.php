ec073a13ec7c99fd925ef5af751db162c477143f
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2022-01-25 16:03:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * @author    Monzurul Hasan <monzurulhasan@gmail.com>
  * @copyright 2022 Ru-Hub
  * @version   v1.0
- * @link      https://github.com/mhs003/PHP-BFInterpreter
+ * @link      https://github.com/mhs003/PHP-BFInterpreter/blob/main/index.php
  * @since     File available since Release 1.0
  * 
  * ***************************************
